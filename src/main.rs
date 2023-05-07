b07b04db9e11b43c40dd18afe81de5069e8977b7
graef <simonjn45@gmail.com>
2023-05-07 14:48:46 +0200

    Got an infinite loop with the IBM rom

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
-// const zero : [u8; 5] = [0xF0, 0x90, 0x90, 0x90, 0xF0];
-// const one  : [u8; 5] = [0x20, 0x60, 0x20, 0x20, 0x70];
-// const two  : [u8; 5] = [0xF0, 0x10, 0xF0, 0x80, 0xF0];
-// const tree : [u8; 5] = [0xF0, 0x10, 0xF0, 0x10, 0xF0];
-// const four : [u8; 5] = [0x90, 0x90, 0xF0, 0x10, 0x10];
-// const five : [u8; 5] = [0xF0, 0x80, 0xF0, 0x10, 0xF0];
-// const six  : [u8; 5] = [0xF0, 0x80, 0xF0, 0x90, 0xF0];
-// const seven: [u8; 5] = [0xF0, 0x10, 0x20, 0x40, 0x40];
-// const eight: [u8; 5] = [0xF0, 0x90, 0xF0, 0x90, 0xF0];
-// const nine : [u8; 5] = [0xF0, 0x90, 0xF0, 0x10, 0xF0];
-// const a    : [u8; 5] = [0xF0, 0x90, 0xF0, 0x90, 0x90];
-// const b    : [u8; 5] = [0xE0, 0x90, 0xE0, 0x90, 0xE0];
-// const c    : [u8; 5] = [0xF0, 0x80, 0x80, 0x80, 0xF0];
-// const d    : [u8; 5] = [0xE0, 0x90, 0x90, 0x90, 0xE0];
-// const e    : [u8; 5] = [0xF0, 0x80, 0xF0, 0x80, 0xF0];
-// const f    : [u8; 5] = [0xF0, 0x80, 0xF0, 0x80, 0x80];
-
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
 const WIDTH: usize = 64;
 const HEIGHT: usize = 32;
+
+pub static FONT_SET: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 1
+    0x20, 0x60, 0x20, 0x20, 0x70, // 2
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 3
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 4
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 5
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 6
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 7
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 8
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // A
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // B
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // C
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // S
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // F
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // G
+];
 
 pub struct Display {
     pub memory: [u8; 2048],
@@ -53,14 +55,31 @@ impl Display {
     }
 
     /// Draw Screen instruction
-    pub fn draw() {
-        // ...
+    pub fn draw(&mut self, x: usize, y: usize, sprite: &[u8]) -> bool {
+        let rows = sprite.len();
+        let mut collision = false;
+        for j in 0..rows {
+            let row = sprite[j];
+            for i in 0..8 {
+                let new_value = row >> (7 - i) & 0x01;
+                if new_value == 1 {
+                    let xi = (x + 1) % WIDTH;
+                    let yi = (y + 1) % HEIGHT;
+                    let old_value = self.get_pixel(xi, yi);
+                    if old_value {
+                        collision = true;
+                    }
+                    self.set_pixel(xi, yi, (new_value == 1) ^ old_value);
+                }
+            }
+        }
+        return  collision;
     }
 }
 
 pub struct Cpu {
     // index register
-    pub l: u16,
+    pub i: u16,
     // program counter
     pub pc: u16,
     // memory
@@ -81,7 +100,7 @@ pub struct Cpu {
 impl Cpu {
     pub fn new() -> Cpu {
         Cpu {
-            l: 0,
+            i: 0,
             pc: 0,
             memory: [0; 4096],
             v: [0; 16],
@@ -92,6 +111,20 @@ impl Cpu {
         }
     }
 
+    pub fn reset(&mut self) {
+        self.i = 0;
+        self.pc = 0x200;
+        self.memory = [0; 4096];
+        self.v = [0; 16];
+        self.stack = [0; 16];
+        self.sp = 0;
+        self.dt = 0;
+        self.display.cls();
+        for i in 0..80 {
+            self.memory[i] = FONT_SET[i];
+        }
+    }
+
     pub fn load_rom(&mut self, file_path: String) -> io::Result<u16> {
         let mut f = File::open(file_path)?;
         let mut buffer = Vec::new();
@@ -99,7 +132,7 @@ impl Cpu {
         // read the whole file
         f.read_to_end(&mut buffer)?;
         for i in 0..buffer.len() {
-            self.memory[i] = buffer[i];
+            self.memory[0x200 + i] = buffer[i];
             // println!("i is {i}");
             // println!("{:>08b}", self.memory[i]);
         }
@@ -113,13 +146,13 @@ impl Cpu {
 
     fn process_opcode(&mut self, opcode: u16) {
         // Extract the parameters
-        // let x = ((opcode & 0x0F00) >> 8) as usize;
-        // let y = ((opcode & 0x00F0) >> 4) as usize;
-        // let vx = self.v[x];
-        // let vy = self.v[y];
-        // let nnn = opcode & 0x0FFF;
-        // let kk = (opcode & 0x00FF) as u8;
-        // let n = (opcode & 0x000F) as u8;
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        let vx = self.v[x];
+        let vy = self.v[y];
+        let nnn = opcode & 0x0FFF;
+        let kk = (opcode & 0x00FF) as u8;
+        let n = (opcode & 0x000F) as u8;
 
         // break up int nibble (1/2 byte)
         let op_1 = (opcode & 0xF000) >> 12;
@@ -130,25 +163,29 @@ impl Cpu {
         // increment the counter
         self.pc += 2;
 
-        // println!("{}, {}, {}, {}", op_1, op_2, op_3, op_4);
+        // println!("{:X}, {:X}, {:X}, {:X}", op_1, op_2, op_3, op_4);
 
         match (op_1, op_2, op_3, op_4) {
             // CLS
             (0, 0, 0xE, 0) => self.display.cls(),
             // RET
             (0, 0, 0xE, 0xE) => todo!("'RET' not implemented yet!"),
-            // JMP
-            (0x1, _, _, _) => todo!("'JMP' not implemented yet!"),
+            // JP addr
+            (0x1, _, _, _) => self.pc = nnn,
             // LD Vx, byte
-            (0x6, _, _, _) => todo!("'LD Vx, byte' not implemented yet!"),
+            (0x6, _, _, _) => self.v[x] = kk,
             // ADD Vx, byte
-            (0x7, _, _, _) => todo!("'ADD Vx, byte' not implemted yet!"),
+            (0x7, _, _, _) => self.v[x] = self.v[x] + kk,
             // LD I, addr
-            (0xA, _, _, _) => todo!("'LD I, addr' not implemented yet!"),
-            // DRW Vx, VY, nibble
-            (0xD, _, _, _) => todo!("'DRW Vx, VY, nibble' not impelmented yet!"),
+            (0xA, _, _, _) => self.i = nnn,
+            // DRW Vx, Vy, nibble
+            (0xD, _, _, _) => {
+                let collision = self.display.draw(vx as usize, vy as usize,
+                    &self.memory[self.i as usize .. (self.i + n as u16) as  usize]);
+                self.v[0xF] = if collision { 1 } else { 0 };
+            },
             // Unknown Opcode
-            (_, _, _, _) => todo!("Unknown opcode"),
+            (_, _, _, _) => (),
         }
     }
 }
@@ -157,12 +194,28 @@ fn read_word(memory: [u8; 4096], index: u16) -> u16 {
     (memory[index as usize] as u16) << 8 | (memory[(index + 1) as usize] as u16)
 }
 
+fn display(mut display: Display) {
+    // for i in 0..display.memory.len() {
+    //     print!("{} ", i);
+    // }
+
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            if display.get_pixel(x, y) {
+                print!("*")
+            } else {
+                print!(" ")
+            }
+        }
+        println!("")
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut chip = Cpu::new();
-    let lenght = chip.load_rom("rom/IBM".to_string()).unwrap();
-    for _ in 0..lenght {
-        chip.execute_cycle ();
+    chip.reset();
+    chip.load_rom("rom/IBM".to_string()).unwrap();
+    loop {
+        chip.execute_cycle();
     }
-    Ok(())
-
 }
