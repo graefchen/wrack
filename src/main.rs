99d3fccba0d4ae65c5ff1cf08f2717f95d9cbe1d
graef <simonjn45@gmail.com>
2023-05-10 21:36:47 +0200

    Added keyboard.

--- src/main.rs
+++ src/main.rs
@@ -97,7 +97,27 @@ impl Display {
 /// Z X C V
 /// Make it scan codes instead of strings
 pub struct Keypad {
+    pub keys : [bool; 16],
+}
 
+impl Keypad {
+    pub fn new() -> Keypad {
+        Keypad {
+            keys: [false; 16]
+        }
+    }
+
+    pub fn key_down(&mut self, index: u8) {
+        self.keys[index as usize] = true;
+    }
+
+    pub fn key_up(&mut self, index: u8) {
+        self.keys[index as usize] = false
+    }
+
+    pub fn is_key_down(&self, index: u8) -> bool {
+        self.keys[index as usize]
+    }
 }
 
 pub struct Cpu {
